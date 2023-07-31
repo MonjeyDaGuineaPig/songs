f7e9744f44cabfea67e1b8eae4e834aae9376017
Monkey_Devin2 <devinrwoods@icloud.com>
2023-07-31 11:55:28 -0400

    Add files via upload

---
+++ 1,2 buckle my shoes by devin woods.rb
@@ -0,0 +1,48 @@
+#"1,2 Buckle my Shoes" by Devin Woods
+
+use_bpm 100
+use_synth :subpulse
+
+
+play :e
+sleep 1
+play :c
+sleep 1
+play :e
+sleep 0.25
+play :e
+sleep 0.25
+play :e
+sleep 0.25
+play :g
+sleep 0.5
+play :e
+sleep 0.75
+play :d
+sleep 1
+play :b3
+sleep 1
+play :d
+sleep 0.25
+play :d
+sleep 0.25
+play :d
+sleep 0.25
+play :f
+sleep 0.5
+play :e
+sleep 0.25
+play :d
+sleep 0.5
+play :e
+sleep 1
+play :c
+sleep 1
+play :e
+sleep 0.5
+play :e
+sleep 0.5
+play :g
+sleep 0.25
+
+#"ho ho ho that was so fire!"
---
+++ drum beat by devin woods.rb
@@ -0,0 +1,18 @@
+#"Basic Drum Beat" by Devin Woods
+use_bpm 1500
+loop do
+  sample :drum_bass_hard
+  sleep 0
+  sample :drum_cymbal_soft
+  sleep 0.5
+  sample :drum_cymbal_soft
+  sleep 0.5
+  sample:drum_snare_soft
+  sleep 0
+  sample :drum_cymbal_soft
+  sleep 0
+  sample :drum_bass_hard
+  sleep 0.5
+  sample :drum_cymbal_soft
+  sleep 0.5
+end
